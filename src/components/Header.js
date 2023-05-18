b23add4d3c45fcf76e6bf6e803698bd9035968aa
Ali Rabiee <ali8181rbi@gmail.com>
2023-05-18 17:08:14 +0330

    complete de boging

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,39 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../image/logo.png";
 
 const Header = () => {
+  const navigat = useNavigate();
   return (
     <div className="navbar bg-base-100 w-full px-10 border-b-2 border-black justify-between">
-      <Link className="btn btn-ghost normal-case text-xl" to={"/"}>
-        <div className="flex-1">
-          <img src={logo} className="-mt-6" />
-        </div>
-      </Link>
+      {/* <Link className="btn btn-ghost normal-case text-xl" to={"/"}> */}
+      <div className="flex-1">
+        <img src={logo} className="-mt-6" onClick={() => navigat("/")} />
+      </div>
+      {/* </Link> */}
       <div className="flex-none w-40">
         <div className="dropdown dropdown-end w-1/2">
           <label tabIndex={0} className="btn btn-ghost btn-circle mx-4">
-            <Link to={"/cart"}>
-              <div className="indicator">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-10 w-10"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                  />
-                </svg>
-                <span className="badge badge-lg indicator-item color border-none">
-                  0
-                </span>
-              </div>
-            </Link>
+            {/* <Link to={"/cart"}> */}
+            <div className="indicator" onClick={() => navigat("/cart")}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-10 w-10"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-lg indicator-item color border-none">
+                0
+              </span>
+            </div>
+            {/* </Link> */}
           </label>
         </div>
         <div className="dropdown dropdown-end w-1/2">
@@ -47,15 +48,15 @@ const Header = () => {
           <ul
             tabIndex={0}
             className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-            <Link className="justify-between" to={"/profile"}>
-              <li>Profile</li>
-            </Link>
-            <Link to={"/settings"}>
-              <li>Settings</li>
-            </Link>
-            <Link to={"/logout"}>
-              <li>Logout</li>
-            </Link>
+            {/* <Link className="justify-between" to={"/profile"}> */}
+            <li onClick={() => navigat("/profile")}>Profile</li>
+            {/* </Link> */}
+            {/* <Link to={"/settings"}> */}
+            <li onClick={() => navigat("/settings")}>Settings</li>
+            {/* </Link> */}
+            {/* <Link to={"/logout"}> */}
+            <li onClick={() => navigat("/logout")}>Logout</li>
+            {/* </Link> */}
           </ul>
         </div>
       </div>
--- src/page/Home.js
+++ src/page/Home.js
@@ -14,42 +14,44 @@ const Home = () => {
 
   return (
     <div className="container grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4">
-      {data.map((item) => (
-        <div
-          className="card w-auto mt-28 mx-5 glass border-none"
-          key={item._id}>
-          <figure
-            className="bg-white border-none"
-            onClick={() => navigat(item._id.toString())}>
-            <img src={item.image} alt="car!" className="border-none" />
-          </figure>
-          <div className="card-body h-72 border-none">
-            <h2
-              className="card-title block text-center"
+      {Object.values(data).map((item) => {
+        return (
+          <div
+            className="card w-auto mt-28 mx-5 glass border-none"
+            key={item._id}>
+            <figure
+              className="bg-white border-none"
               onClick={() => navigat(item._id.toString())}>
-              {item.name}
-            </h2>
-            <span
-              onClick={() => navigat(item._id.toString())}
-              className="badge badge-lg indicator-item rounded-full text-2xl font-bold w-12 h-12 absolute -top-5 -right-5 color border-none">
-              {item.countInStock}
-            </span>
-            <span onClick={() => navigat(item._id.toString())}>
-              {item.price} $
-            </span>
-            <span onClick={() => navigat(item._id.toString())}>
-              rating: {item.rating}
-            </span>
-            <div className="card-actions justify-end">
-              <button
-                className="btn absolute bottom-7 right-5 color border-none"
-                onClick={() => navigat("/cart")}>
-                Add to Cart
-              </button>
+              <img src={item.image} alt="car!" className="border-none" />
+            </figure>
+            <div className="card-body h-72 border-none">
+              <h2
+                className="card-title block text-center"
+                onClick={() => navigat(item._id.toString())}>
+                {item.name}
+              </h2>
+              <span
+                onClick={() => navigat(item._id.toString())}
+                className="badge badge-lg indicator-item rounded-full text-2xl font-bold w-12 h-12 absolute -top-5 -right-5 color border-none">
+                {item.countInStock}
+              </span>
+              <span onClick={() => navigat(item._id.toString())}>
+                {item.price} $
+              </span>
+              <span onClick={() => navigat(item._id.toString())}>
+                rating: {item.rating}
+              </span>
+              <div className="card-actions justify-end">
+                <button
+                  className="btn absolute bottom-7 right-5 color border-none"
+                  onClick={() => navigat("/cart")}>
+                  Add to Cart
+                </button>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
